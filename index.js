8f3e6e5017222f609253eda1acf3da365ca5798b
Bernard Legaspi <blegaspi26@gmail.com>
2023-04-07 17:57:35 -0400

    commit tree 70fe66c4d07b8459c1aa4058314b12880a6b7249

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function asdf() {
+    console.log("asdf");
+    let num = 0;
+    num += 1;
+}
+
+asdf();
+
+
+
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
+
+function distanceFromHqInFeet(someValue) {
+    // const distance = someValue;
+    return 264 * distanceFromHqInBlocks(someValue);
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+}
+
+function calculatesFarePrice(start, end) {
+    const distance = distanceTravelledInFeet(start, end);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance < 2000) {
+        return 0.02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else if (distance > 2500) {
+        return "cannot travel that far";
+    }
+} 
